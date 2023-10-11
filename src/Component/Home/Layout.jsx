0aa37738e21a0a8b5e26fc7984c92a8966b6edec
Bhavesh Zalke <zalkebhavesh@gmail.com>
2023-10-11 16:59:21 +0530

    Cleanup

--- src/Component/Home/Layout.jsx
+++ src/Component/Home/Layout.jsx
@@ -1,15 +1,4 @@
-// import { Link } from 'react-router-dom';
-// import './Layout.css'
-// function Layout({ title, url, id }) {
-//     return (
-//         <div className="LayoutWrapper">
-//             <Link to={`/Single/:${id}`}>
-//                 <img src={url} alt="" className='BirdImg' />
-//             </Link>
-//         </div>
-//     )
-// }
-// export default Layout;
+
 
 
 import React from "react"
--- src/Component/Routes/CustomeRoute.jsx
+++ src/Component/Routes/CustomeRoute.jsx
@@ -1,17 +1,4 @@
-// import { Route, Routes } from "react-router-dom";
-// import Home from "../Home/Home";
-// import Single from "../Single/Single";
-// function CustomRoute() {
-//     return (
-//         <>
-//             <Routes>
-//                 <Route path="/" element={<Home/>} />
-//                 <Route path="/Home/:id" element={<Single/>} />
-//             </Routes>
-//         </>
-//     )
-// }
-// export default CustomRoute;
+
 
 import { Route, Routes } from "react-router-dom"
 import {Home} from '../Home/Home'
